1e06e41675603c0b826fb697688b0691f727b17e
official <zazzzazazazazaza.09@gmail.com>
2026-06-21 16:24:28 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -406,7 +406,7 @@ function handleDrawerAction(action) {
   closeDrawer();
   switch (action) {
     case "home": window.scrollTo({ top: 0, behavior: "smooth" }); break;
-    case "preview": window.open("https://mega.nz/folder/DzpX0LaK#oNRH_UOngK6IYqlpYTWkkQ", "_blank", "noopener,noreferrer"); break;
+    case "preview": window.open("https://mega.nz/folder/uCohnZjY#PXR2kVHZCl3lv69iq-evvg", "_blank", "noopener,noreferrer"); break;
     case "menu": case "more-videos": switchPanelSection("custom"); break;
     case "invites": switchPanelSection("invites"); break;
     case "reviews": alert("Reviews section is coming soon."); break;
